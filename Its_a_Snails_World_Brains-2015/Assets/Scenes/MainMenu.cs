6bc664a1d79b71bc8241b1864eacf21ad4a8ef8e
TomZoy <mr.tomzoy@gmail.com>
2015-06-28 00:53:29 +0100

    aaa
    ase

---
+++ Its_a_Snails_World_Brains-2015/Assets/Scenes/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void playButton()
+    {
+        Application.LoadLevel("Level1");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+   // public void optionsButton()
+    //{
+     //   Application.LoadLevel("Options");
+    //}
+    //hello zoltan, this better work
+}
+
